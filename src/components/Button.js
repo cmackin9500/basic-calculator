1b09b13a4b6cf57a83ec50b3a6428dbe431eb3ba
cmackin9500 <casey@mackinnon.org>
2023-01-25 13:47:31 -0500

    all functionality done. might add some more

--- src/components/Button.js
+++ src/components/Button.js
@@ -18,18 +18,97 @@ const getStyleName = (btn) => {
 const Button = ({ value }) => {
     const { calc, setCalc } = useContext(CalcContext);
 
+    // user clicks decimal
     const decimalClick = () => {
         setCalc({
             ...calc,
-            num: 29
+            num: !calc.num.toString().includes('.') ? calc.num+value : calc.num
+        })
+    }
+
+    // user clicks AC or clear
+    const clearClick = () => {
+        setCalc({
+            sign: "",
+            num: 0,
+            res: 0
+        })
+    }
+
+    // user clicks number
+    const numClick = () => {
+        let tempNum = value.toString();
+        let newNum;
+        if (tempNum === "0" && calc.num === 0)
+            newNum = "0";
+        else
+            newNum = Number(calc.num + tempNum);
+
+        setCalc({
+            ...calc,
+            num: newNum
+        })
+    }
+
+    // user clicks on of the operations
+    const opClick = () => {
+        setCalc({
+            sign: value,
+            res: calc.num && !calc.res ? calc.num : calc.res,
+            num: 0
+        })
+    }
+
+    // user clicks equal sign
+    const equalClick = () => {
+        const calculate = (res,num,sign) => {
+            const result = {
+                '+': (res,num) => res+num,
+                '-': (res,num) => res-num,
+                'x': (res,num) => res*num,
+                '÷': (res,num) => res/num
+            }
+            return result[sign](res,num);
+        }
+
+        setCalc({
+            res: calculate(calc.res, calc.num, calc.sign),
+            sign: "",
+            num: 0
+        })
+    }
+
+    // user clicks +/-
+    const plusMinusClick = () => {
+        setCalc({
+            sign: "",
+            num: -calc.num,
+            res: -calc.res
+        })
+    }
+
+    // user clicks %
+    const percentClick = () => {
+        setCalc({
+            sign: "",
+            num: calc.num / 100,
+            res: calc.res / 100
         })
     }
 
     const handleButtonClick = () => {
         const results = {
-            '.': decimalClick
+            '.': decimalClick,
+            'AC': clearClick,
+            '÷': opClick,
+            'x': opClick,
+            '+': opClick,
+            '-': opClick,
+            '=': equalClick,
+            '+/-': plusMinusClick,
+            '%': percentClick,
         }
-        return results[value]();
+        return value in results ? results[value]() : numClick()
     }
 
     return (
